a707f138add039c1915d366108f17d4e50de4f78
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2025-01-30 19:18:47 +0300

    footer design fix

--- ottomania cafe/src/components/Footer.jsx
+++ ottomania cafe/src/components/Footer.jsx
@@ -2,19 +2,23 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mx-8 text-white p-4 text-center text-sm">
-      <p>© {new Date().getFullYear()} Tüm Hakları Saklıdır.</p>
-      <p>
-        Bu site{" "}
-        <a
-          href="https://github.com/seninprofilin"
-          target="_blank"
-          className="text-blue-400 hover:underline"
-        >
-          Senin Adın
-        </a>{" "}
-        tarafından yapılmıştır.
-      </p>
+    <div className="mx-8 text-white p-4  text-sm border-t-1">
+      <div className="flex"></div>{" "}
+      {/*Facebook instagram X vs. sosyal medya linkleri */}
+      <div className="text-center">
+        <p>© {new Date().getFullYear()} Tüm Hakları Saklıdır.</p>
+        <p>
+          Bu site{" "}
+          <a
+            href="https://github.com/seninprofilin"
+            target="_blank"
+            className="text-blue-400 hover:underline"
+          >
+            Senin Adın
+          </a>{" "}
+          tarafından yapılmıştır.
+        </p>
+      </div>
     </div>
   );
 };
