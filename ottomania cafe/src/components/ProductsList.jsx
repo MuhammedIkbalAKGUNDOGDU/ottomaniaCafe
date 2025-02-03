39581cc8ebf57f7d1d262882f9e88405b2357da1
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2025-02-04 01:10:17 +0300

    fix2

--- ottomania cafe/src/components/ProductsList.jsx
+++ ottomania cafe/src/components/ProductsList.jsx
@@ -4,7 +4,6 @@ import LogoGarden from "../assets/garden.png";
 
 const ProductsList = ({ list, nameOfCafe }) => {
   // nameOfCafe değerine göre doğru logoyu belirle
-  console.log(nameOfCafe);
   const Logo = nameOfCafe === "Ottomania Cafe" ? LogoCafe : LogoGarden;
 
   return (
@@ -12,7 +11,7 @@ const ProductsList = ({ list, nameOfCafe }) => {
       {list.map((item, index) => (
         <div
           key={index}
-          className="bg-[#3A3E40] text-white p-4 mt-4 rounded-lg shadow-md h-100 text-center mx-8 flex items-center justify-center flex-col"
+          className="bg-[#3A3E40] text-white p-4 mt-4 rounded-lg shadow-md h-96 text-center mx-8 flex items-center justify-center flex-col"
         >
           <h2 className="text-4xl text-white font-bold capitalize">{item.name}</h2>
           <p className="text-white mt-2">{item.description}</p>
